8261deed40a0743d74a4cbdd9a8a012de5947562
DavideNicoletta <davidenicoletta@gmail.com>
2023-06-18 16:57:13 +0200

    Modifica prolog

--- Prolog/mvpoly.pl
+++ Prolog/mvpoly.pl
@@ -3,17 +3,19 @@
 
 
 
-%%% is_monomial/1 
+%%% is_monomial/1
+is_monomial(poly(_)) :-
+    false.
 is_monomial(m(_C, TD, VPs)) :-
     integer(TD),
     TD >= 0,
     is_list(VPs)
     foreach(member(V, VPs), is_varpower(V)),
-    sum_degrees_variables(VPs, TD).
+    sum_degrees(VPs, TD).
 
 %%% sum_degrees/2
 sum_degrees([], 0) :- !.
-sum_degrees([v(Exponent, Variable) | Vs], TotalDegree) :-
+sum_degrees([v(Exponent, _Variable) | Vs], TotalDegree) :-
     sum_degrees(Vs, TotalDegree2), !,
     TotalDegree is Exponent + TotalDegree2.
     
@@ -31,7 +33,7 @@ as_monomial(Expression, Monomial) :-
 
 
 %%% monomial_sorted/2
-monomial_sorted(Expression, m(C, TD, VPs) :-
+monomial_sorted(Expression, m(C, TD, VPs)) :-
 		    monomial_start(Expression, m(C, TD, VPs2)),
 		    sort(2, @=<, VPs2, VPs).
 
@@ -123,7 +125,7 @@ is_polynomial(Monomial) :-
 
 
 %%% as_polynomial/2
-as_polynomail(Expression, Polynomial) :-
+as_polynomial(Expression, Polynomial) :-
     polynomial_sorted(Expression, PolynomialZero),
     remove_zero(PolynomialZero, Polynomial).
 
@@ -136,24 +138,109 @@ polynomial_sorted(m(C, TD, VPs2), poly([m(C, TD, VPs])) :-
 		      sort(2, @=<, VPs2, VPs).
 polynomial_sorted(Expression, poly(Monomials)) :-
     polynomial_start(Expression, poly(Monomial)),
-    
-    
+    sort_polynomial(poly(Monomial), poly(SortedMonomials)),
+    sum_monomials(poly(SortedMonomials), poly(Monomials)).
+
+
+%%% sort_polynomials/2
+sort_polynomials(poly(Monomials), poly(SortedMonomials)) :-
+    remove_zero(poly(Monomials), poly(MonomialsZero)),
+    predsort(compare_monomials, MonomialsZero, SortedMonomials).
+
+
+%%% remove_zero/2
+remove_zero(poly([]), poly([])) :-
+    !.
+remove_zero(poly([m(0, _, _) | Tail]), poly(Tail2)) :-
+    !,
+    remove_zero(poly(Tail), poly(Tail2)).
+remove_zero(poly([m(C, TD, VPs) | Tail], poly([m(C, TD, VPs) | Tail2])) :-
+		!,
+		remove_zero(poly(Tail), poly(Tail2)).
+
+
+%%% polynomial_start/2
+polynomial_start(Head + Tail, poly(Parsed)) :-
+    as_monomial(Tail, ParsedTail),
+    polynomila_start(Head, poly(ParsedHead)),
+    !,
+    append([ParsedTail], ParsedHead, Parsed).
+polynomial_start(Head - Tail, poly(Parsed)) :-
+    as_monomial(-Tail, ParsedTail),
+    polynomial_start(Head, poly(ParsedHead)),
+    !,
+    append([ParsedTail], ParsedHead, Parsed).
+polynomial_start(Monomial, poly([ParsedMono])) :-
+    !,
+    as_monomial(Monomial, ParsedMono).
+
+
+%%% sum_monomials/2
+sum_monomials(poly([]), poly([])) :-
+    !.
+sum_monomials(poly([X]), poly([X])) :-
+    !.
+sum_monomials(poly([m(C, TD, VPs), m(D, TD, VPs) | Tail1]), poly(Tail2)) :-
+    !,
+    X is C + D,
+    !,
+    sum_monomials(poly([m(X, TD, VPs) | Tail1]), poly(Tail2)).
+sum_monomials(poly([A, B | Tail1]), poly([A | Tail2])) :-
+    !,
+    sum_monomials(poly([B | Tail1]), poly(Tail2)).
+
 
 %%% coefficients/2
 %%% coefficients(Poly, Coefficients)
 %%% true whene Coefficients is a list composed of
 %%% the coefficients of polinomial
 
-coefficients(poly([]), [0]).
+coefficients(poly([]), [0]) :-
+    !.
+coefficients(Polynomial, Coefficients) :-
+    red_sort_polynomial(Polynomial, PolynomialParsed),
+    !,
+    get_polynomial_coefficients(PolynomialParsed, Coefficients).
 
-coefficients(poly([M | Ms]), Coefficients) :-
-    coefficients(poly(Ms), [_ | Coefficients]),
-    coefficients(poly(M), Coefficients).
+
+%%% is_mono_parse/1
+is_mono_parse(m(_, _, _)) :-
+    !.
+
+%%% red_sort_polynomial/2
+red_sort_polynomail(Monomial, ParsedPolynomial) :-
+    is_monomial(Monomial),
+    is_mono_parse(Monomial),
+    !,
+    as_polynomial(Monomial, ParsedPolynomial).
+red_sort_polynomial(Polynomial, ParsedPolynomial) :-
+    is_polynomial(Polynomial),
+    sort_polynomial(Polynomial, Sort),
+    sum_monomials(Sort, Sum),
+    remove_zero(Sum, ParsedPolynomial).
+red_sort_polynomial(Polynomial, ParsedPolynomial) :-
+   as_polynomial(Polynomial, ParsedPolynomial).
+
+
+%%% get_polynomial_coefficients/2
+get_polynomial_coefficients(poly([]), []) :-
+    !.
+get_polynomial_coefficients(poly(Monomials), Coefficients) :-
+    get_polynomial_coefficients(.
+
+
+%%% get_polynomial_coefficients_execute/2
+get_polynomial_coefficients_execute(poly([]), []) :-
+    !.
+get_polynomial_coefficients_execute(poly([Head | Rest]), [R | RestCoef]) :-
+    get_coefficient_mono(Head, R),
+    get_coefficient_polynomial_execute(poly(Rest=, RestCoef).
     
-coefficients(poly([m(C, _, _)]), C):- !.
 
-
-
+%%% get_coefficient_mono/2
+get_coefficient_mono(m(C, _, _), C) :-
+    !.
+    
     
 
 %%% 
